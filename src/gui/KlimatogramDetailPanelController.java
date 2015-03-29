f2b32d14b358d0d541e4ea473e8887a8655fe97b
Jan Van Braeckel <jan.vanbraeckel.x7253@student.hogent.be>
2015-03-29 19:39:40 +0200

    UC-CreëerKlimatogramgegevens: Ontwerp UI

---
+++ src/gui/KlimatogramDetailPanelController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Annemie
+ */
+public class KlimatogramDetailPanelController extends Pane {
+
+    public KlimatogramDetailPanelController() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("KlimatogramDetailPanel.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
