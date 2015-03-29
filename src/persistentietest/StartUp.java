d3cb83ff2b8a465e244d35490b7cc07f10ce3121
Jan Van Braeckel <jan.vanbraeckel.x7253@student.hogent.be>
2015-03-29 13:45:42 +0200

    UC-CreëerKlimatogramgegevens: Grafiek klimatogram implementeren

--- src/persistentietest/StartUp.java
+++ src/persistentietest/StartUp.java
@@ -9,20 +9,20 @@ import java.util.List;
  * @author Jasper De Vrient
  */
 public class StartUp {
-    
+
     public static void main(String[] args) {
         System.out.println("hier");
         GenericDao<Continent, String> dao = new GenericDaoJpa<>(Continent.class);
-        
+
        
         
-            List<Continent> continents = dao.getAll();
-   
-         
-            
-            for (Continent c : continents)
-                System.out.println(c.getNaam());
+        List<Continent> continents = dao.getAll();
+
         
+  
+            for (Continent c : continents)
+            System.out.println(c.getNaam());
+
         GenericDaoJpa.closePersistency();
     }
 }
