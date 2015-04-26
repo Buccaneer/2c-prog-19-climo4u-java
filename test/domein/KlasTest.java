7547ff6ab861dafa60f8ff202227c1d8ae806e20
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2015-04-26 18:27:12 +0200

    #5 testen

--- test/domein/KlasTest.java
+++ test/domein/KlasTest.java
@@ -1,10 +1,28 @@
 package domein;
 
+import org.junit.Test;
+
 /**
  *
  * @author Pieter-Jan
  */
 public class KlasTest
 {
+    @Test(expected=IllegalArgumentException.class)
+    public void setNaamNullGooitException()
+    {
+        new Klas().setNaam(null);
+    }
     
+    @Test(expected=IllegalArgumentException.class)
+    public void setLeerjaarNegatiefGooitException()
+    {
+        new Klas().setLeerjaar(-1);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void setGraadNullGooitException()
+    {
+        new Klas().setGraad(null);
+    }
 }
--- test/domein/LeerlingTest.java
+++ test/domein/LeerlingTest.java
@@ -1,10 +1,30 @@
 package domein;
 
+import org.junit.Test;
+
 /**
  *
  * @author Pieter-Jan
  */
 public class LeerlingTest
 {
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void setNaamNullGooitException()
+    {
+        new Leerling().setNaam(null);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void setVoorNaamNullGooitException()
+    {
+        new Leerling().setVoornaam(null);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void setKlasNullGooitException()
+    {
+        new Leerling().setKlas(null);
+    }
     
 }
